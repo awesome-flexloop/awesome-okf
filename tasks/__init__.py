d672359046cea794aaf17bfac593166400b6c313
xinetzone <735613050@qq.com>
2026-09-04 10:18:10 +0800

    feat(build-invs): build_invs+自定义inv builder(Sphinx9)+ns注册 [prevent: test-case]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,8 +4,6 @@ CLI 向后兼容：
     invoke build / invoke clean / invoke browse / invoke tree / invoke doctest
     invoke gates.utf8 / invoke gates.toctrees / invoke gates.all
 """
-from __future__ import annotations
-
 from invoke import Collection
 
 from . import docs, gates
@@ -18,6 +16,7 @@ ns.add_task(docs.clean)
 ns.add_task(docs.browse)
 ns.add_task(docs.tree)
 ns.add_task(docs.doctest)
+ns.add_task(docs.build_invs)
 
 # 质量门任务作为子集合：invoke gates.utf8 / invoke gates.toctrees
 ns.add_collection(Collection.from_module(gates))
